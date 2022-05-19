6b1e719d9f6d2dce3d4724535ace67fc62dcb0a7
TinTinaA <101172637+yaboidimsum@users.noreply.github.com>
2022-05-19 17:55:35 +0700

    Hidangan

---
+++ Hidangan.c
@@ -0,0 +1,457 @@
+/*Dimas Prihady Setyawan 5025211184 */
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int answer = 0;
+
+typedef struct AVLNode_t
+{
+    int data;
+    struct AVLNode_t *left,*right;
+    int height;
+}AVLNode;
+
+typedef struct AVL_t
+{
+    AVLNode *_root;
+    unsigned int _size;
+}AVL;
+
+AVLNode* _avl_createNode(int value) {
+    AVLNode *newNode = (AVLNode*) malloc(sizeof(AVLNode));
+    newNode->data = value;
+    newNode->height=1;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+AVLNode* _search(AVLNode *root, int value) {
+    while (root != NULL) {
+        if (value < root->data)
+            root = root->left;
+        else if (value > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return root;
+}
+
+int _getHeight(AVLNode* node){
+    if(node==NULL)
+        return 0;
+    return node->height;
+}
+
+int _max(int a,int b){
+    return (a > b)? a : b;
+}
+
+AVLNode* _rightRotate(AVLNode* pivotNode) {
+
+    AVLNode* newParrent=pivotNode->left;
+    pivotNode->left=newParrent->right;
+    newParrent->right=pivotNode;
+
+    pivotNode->height=_max(_getHeight(pivotNode->left),
+                      _getHeight(pivotNode->right))+1;
+    newParrent->height=_max(_getHeight(newParrent->left),
+                       _getHeight(newParrent->right))+1;
+
+    return newParrent;
+}
+
+AVLNode* _leftRotate(AVLNode* pivotNode) {
+
+    AVLNode* newParrent=pivotNode->right;
+    pivotNode->right=newParrent->left;
+    newParrent->left=pivotNode;
+
+    pivotNode->height=_max(_getHeight(pivotNode->left),
+                      _getHeight(pivotNode->right))+1;
+    newParrent->height=_max(_getHeight(newParrent->left),
+                       _getHeight(newParrent->right))+1;
+
+    return newParrent;
+}
+
+AVLNode* _leftCaseRotate(AVLNode* node){
+    return _rightRotate(node);
+}
+
+AVLNode* _rightCaseRotate(AVLNode* node){
+    return _leftRotate(node);
+}
+
+AVLNode* _leftRightCaseRotate(AVLNode* node){
+    node->left=_leftRotate(node->left);
+    return _rightRotate(node);
+}
+
+AVLNode* _rightLeftCaseRotate(AVLNode* node){
+    node->right=_rightRotate(node->right);
+    return _leftRotate(node);
+}
+
+int _getBalanceFactor(AVLNode* node){
+    if(node==NULL)
+        return 0;
+    return _getHeight(node->left)-_getHeight(node->right);
+}
+
+AVLNode* _insert_AVL(AVL *avl,AVLNode* node,int value) {
+
+    if(node==NULL) // udah mencapai leaf
+        return _avl_createNode(value);
+    if(value < node->data)
+        node->left = _insert_AVL(avl,node->left,value);
+    else if(value > node->data)
+    	node->right = _insert_AVL(avl,node->right,value);
+
+    node->height= 1 + _max(_getHeight(node->left),_getHeight(node->right));
+
+    int balanceFactor=_getBalanceFactor(node);
+
+    if(balanceFactor > 1 && value < node->left->data) // skewed kiri (left-left case)
+        return _leftCaseRotate(node);
+    if(balanceFactor > 1 && value > node->left->data) //
+		return _leftRightCaseRotate(node);
+    if(balanceFactor < -1 && value > node->right->data)
+        return _rightCaseRotate(node);
+    if(balanceFactor < -1 && value < node->right->data)
+        return _rightLeftCaseRotate(node);
+
+    return node;
+}
+
+AVLNode* _findMinNode(AVLNode *node) {
+    AVLNode *currNode = node;
+    while (currNode && currNode->left != NULL)
+        currNode = currNode->left;
+    return currNode;
+}
+
+AVLNode* _remove_AVL(AVLNode* node,int value){
+    if(node==NULL)
+        return node;
+    if(value > node->data)
+    	node->right=_remove_AVL(node->right,value);
+    else if(value < node->data)
+    	node->left=_remove_AVL(node->left,value);
+    else{
+        AVLNode *temp;
+        if((node->left==NULL)||(node->right==NULL)){
+            temp=NULL;
+            if(node->left==NULL) temp=node->right;
+            else if(node->right==NULL) temp=node->left;
+
+            if(temp==NULL){
+                temp=node;
+                node=NULL;
+            }
+            else
+                *node=*temp;
+
+            free(temp);
+        }
+        else{
+            temp = _findMinNode(node->right);
+            node->data=temp->data;
+            node->right=_remove_AVL(node->right,temp->data);
+        }
+    }
+
+	if(node==NULL) return node;
+
+    node->height=_max(_getHeight(node->left),_getHeight(node->right))+1;
+
+    int balanceFactor= _getBalanceFactor(node);
+
+    if(balanceFactor>1 && _getBalanceFactor(node->left)>=0)
+        return _leftCaseRotate(node);
+
+    if(balanceFactor>1 && _getBalanceFactor(node->left)<0)
+        return _leftRightCaseRotate(node);
+
+    if(balanceFactor < -1 && _getBalanceFactor(node->right)<=0)
+        return _rightCaseRotate(node);
+
+    if(balanceFactor < -1 && _getBalanceFactor(node->right)>0)
+        return _rightLeftCaseRotate(node);
+
+    return node;
+}
+
+void avl_init(AVL *avl) {
+    avl->_root = NULL;
+    avl->_size = 0u;
+}
+
+bool avl_isEmpty(AVL *avl) {
+    return avl->_root == NULL;
+}
+
+bool avl_find(AVL *avl, int value) {
+    AVLNode *temp = _search(avl->_root, value);
+    if (temp == NULL)
+        return false;
+
+    if (temp->data == value)
+        return true;
+    else
+        return false;
+}
+
+void avl_insert(AVL *avl,int value){
+    if(!avl_find(avl,value)){
+        avl->_root=_insert_AVL(avl,avl->_root,value);
+        avl->_size++;
+    }
+
+}
+
+void avl_remove(AVL *avl,int value){
+    if(avl_find(avl,value)){
+        avl->_root=_remove_AVL(avl->_root,value);
+        avl->_size--;
+    }
+
+}
+
+void preorder(AVLNode *root) {
+    if (root) {
+        preorder(root->left);
+        printf("%d ", root->data);
+        preorder(root->right);
+    }
+}
+
+/**
+ * Implementasi ADT Deque (Double-ended Queue)
+ *
+ * Dibuat dan ditulis oleh Bayu Laksana
+ * -- tanggal 22 Januari 2019
+ * Struktur Data 2020
+ * Implementasi untuk bahasa C
+ *
+ * !!NOTE!!
+ * cara menggunakan lihat pada fungsi main()
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Struktur Node */
+
+typedef struct dnode_t {
+    int data;
+    struct dnode_t      \
+        *next,
+        *prev;
+} DListNode;
+
+/* Struktur ADT Deque */
+/* Implementasi didasarkan pada Doubly-Linked List */
+
+typedef struct deque_t {
+    DListNode           \
+        *_head,
+        *_tail;
+    unsigned _size;
+} Deque;
+
+/* DAFTAR FUNGSI YANG TERSEDIA */
+
+DListNode* __dlist_createNode(int value);
+void deq_init(Deque *deque);
+bool deq_isEmpty(Deque *deque);
+void deq_pushFront(Deque *deque, int value);
+void deq_pushBack(Deque *deque, int value);
+int  deq_front(Deque *deque);
+int  deq_back(Deque *deque);
+void deq_popFront(Deque *deque);
+void deq_popBack(Deque *deque);
+
+/* Definisi Fungsi */
+
+DListNode* __dlist_createNode(int value)
+{
+    DListNode *newNode = \
+        (DListNode*) malloc (sizeof(DListNode));
+
+    if (!newNode) return NULL;
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    return (DListNode*) newNode;
+}
+
+void deq_init(Deque *deque)
+{
+    deque->_head = deque->_tail = NULL;
+    deque->_size = (unsigned) 0;
+}
+
+bool deq_isEmpty(Deque *deque) {
+    return (deque->_head == NULL && \
+            deque->_tail == NULL);
+}
+
+void deq_pushFront(Deque *deque, int value)
+{
+    DListNode *newNode = __dlist_createNode(value);
+    if (newNode) {
+        deque->_size++;
+        if (deq_isEmpty(deque)) {
+            deque->_head = newNode;
+            deque->_tail = newNode;
+            return;
+        }
+
+        newNode->next = deque->_head;
+        deque->_head->prev = newNode;
+        deque->_head = newNode;
+    }
+}
+
+void deq_pushBack(Deque *deque, int value)
+{
+    DListNode *newNode = __dlist_createNode(value);
+    if (newNode) {
+        deque->_size++;
+        if (deq_isEmpty(deque)) {
+            deque->_head = newNode;
+            deque->_tail = newNode;
+            return;
+        }
+
+        deque->_tail->next = newNode;
+        newNode->prev = deque->_tail;
+        deque->_tail = newNode;
+    }
+}
+
+int deq_front(Deque *deque) {
+    if (!deq_isEmpty(deque)) {
+        return (deque->_head->data);
+    }
+    return 0;
+}
+
+int deq_back(Deque *deque) {
+    if (!deq_isEmpty(deque)) {
+        return (deque->_tail->data);
+    }
+    return 0;
+}
+
+void deq_popFront(Deque *deque)
+{
+    if (!deq_isEmpty(deque)) {
+        DListNode *temp = deque->_head;
+        if (deque->_head == deque->_tail) {
+            deque->_head = NULL;
+            deque->_tail = NULL;
+            free(temp);
+        }
+        else {
+            deque->_head = deque->_head->next;
+            deque->_head->prev = NULL;
+            free(temp);
+        }
+        deque->_size--;
+    }
+}
+
+void deq_popBack(Deque *deque)
+{
+    if (!deq_isEmpty(deque)) {
+        DListNode *temp;
+        if (deque->_head == deque->_tail) {
+            temp = deque->_head;
+            deque->_head = NULL;
+            deque->_tail = NULL;
+            free(temp);
+        }
+        else {
+            temp = deque->_tail;
+            deque->_tail = deque->_tail->prev;
+            deque->_tail->next = NULL;
+            free(temp);
+        }
+        deque->_size--;
+    }
+}
+
+void verticalSumDLLUtil(AVLNode *root, DListNode *sumNode){
+
+    sumNode->data = sumNode->data + root->data;
+
+    if(root->left)
+    {
+        if(sumNode->prev == NULL)
+        {
+            sumNode->prev = __dlist_createNode(0);
+            sumNode->prev->next = sumNode;
+        }
+        verticalSumDLLUtil(root->left, sumNode->prev);
+    }
+
+    if(root->right)
+    {
+        if(sumNode->next == NULL)
+        {
+            sumNode->next = __dlist_createNode(0);
+            sumNode->next->prev = sumNode;
+        }
+        verticalSumDLLUtil(root->right, sumNode->next);
+    }
+}
+
+
+void verticalSumDLL(AVLNode* root)
+{
+    DListNode* sumNode = __dlist_createNode(0);
+
+    verticalSumDLLUtil(root, sumNode);
+
+    while(sumNode->prev != NULL){
+        sumNode = sumNode->prev;
+    }
+
+    while(sumNode != NULL){
+//        printf("%d ", sumNode->data);
+        answer = answer + pow(sumNode->data,2);
+        sumNode = sumNode->next;
+    }
+}
+
+int main(){
+    AVL avltree;
+    avl_init(&avltree);
+
+    char order[100];
+    int x;
+    bool end = false;
+
+    while (!end) {
+        scanf("%s", order);
+        if (strcmp(order, "MASUKKAN") == 0) {
+            scanf("%d", &x);
+            avl_insert(&avltree, x);
+        } else {
+            if(avltree._root == NULL){
+                printf("0\n");
+                break;
+            }
+            verticalSumDLL(avltree._root);
+            printf("%d", answer);
+            break;
+        }
+    }
+}
